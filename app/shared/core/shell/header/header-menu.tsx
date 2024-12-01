5fae07034d2aaa8dccdebb9e6769628d489822df
Asefa Ayalew <asefa12a19@gmail.com>
2024-12-01 21:48:05 +0300

    Minor change

--- app/shared/core/shell/header/header-menu.tsx
+++ app/shared/core/shell/header/header-menu.tsx
@@ -16,15 +16,7 @@ import {
 import { useTranslation } from "react-i18next";
 import { useRouter } from "next/navigation";
 import classes from "./header-menu.module.css";
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-if (!supabaseUrl || !supabaseKey) {
-    throw new Error("Supabase environment variables are missing!");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabase } from "@/app/shared/ui/auth/login/supabase-client";
 
 
 // Define types for menu items
